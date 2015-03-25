18f7039ec1756d44ebd81876169716adb4da7091
Yao Xin <yaox12@mails.tsinghua.edu.cn>
2015-03-25 14:37:26 +0800

    04-1-spoc

---
+++ src/04-1-spoc/locality2.cpp
@@ -0,0 +1,14 @@
+#include <cstdio>
+#define NUM 1024
+#define COUNT 10
+
+int A[NUM][NUM];
+
+int main () {
+	int i, j, k;
+  	for (k = 0; k < COUNT; k++)
+		for (i = 0; i < NUM; i++)
+  			for (j = 0; j < NUM; j++)
+      			A[j][i] = i + j;
+  	printf("%d count computing over!\n", i*j*k);
+}
